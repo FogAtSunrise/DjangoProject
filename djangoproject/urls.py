dec63ca189c59aaceac22de7e47fcc7f210bd7a5
Верещагина Ирина Александровна <hjr462@mail.ru>
2022-04-23 02:52:49 +0700

    pravki

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -1,4 +1,4 @@
-"""django_knit URL Configuration
+"""djangoproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
